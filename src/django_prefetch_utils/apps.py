f27e97cb7cfcd8e12c825111cff90b47e9928c8f
Mike Hansen <mike@rover.com>
2019-06-06 12:03:58 +0200

    Allow setting the default implementation from settings

--- src/django_prefetch_utils/apps.py
+++ src/django_prefetch_utils/apps.py
@@ -1,4 +1,11 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+try:
+    basestring
+except NameError:
+    basestring = str
 
 
 class DjangoPrefetchUtilsAppConfig(AppConfig):
@@ -9,3 +16,17 @@ class DjangoPrefetchUtilsAppConfig(AppConfig):
             enable_prefetch_related_objects_selector
         )
         enable_prefetch_related_objects_selector()
+        self.set_default_prefetch_related_objects_implementation()
+
+    def set_default_prefetch_related_objects_implementation(self):
+        from django_prefetch_utils.selector import (
+            set_default_prefetch_related_objects
+        )
+        selected = getattr(settings, 'PREFETCH_UTILS_DEFAULT_IMPLEMENTATION', None)
+        if selected is None:
+            return
+
+        if isinstance(selected, basestring):
+            selected = import_string(selected)
+
+        set_default_prefetch_related_objects(selected)
---
+++ tests/apps_tests.py
@@ -0,0 +1,37 @@
+from django.apps import apps
+from django.test import TestCase
+from django.test import override_settings
+
+from django_prefetch_utils.identity_map import prefetch_related_objects as identity_map_prefetch_related_objects
+from django_prefetch_utils.selector import get_prefetch_related_objects
+from django_prefetch_utils.selector import original_prefetch_related_objects
+from django_prefetch_utils.selector import remove_default_prefetch_related_objects
+
+
+class DjangoPrefetchUtilsAppConfigTests(TestCase):
+    def setUp(self):
+        self.config = apps.get_app_config("django_prefetch_utils")
+        self.addCleanup(remove_default_prefetch_related_objects)
+
+    @override_settings(
+        PREFETCH_UTILS_DEFAULT_IMPLEMENTATION="django_prefetch_utils.identity_map.prefetch_related_objects"
+    )
+    def test_default_from_string(self):
+        self.config.set_default_prefetch_related_objects_implementation()
+        self.assertIs(
+            get_prefetch_related_objects(), identity_map_prefetch_related_objects
+        )
+
+    @override_settings(
+        PREFETCH_UTILS_DEFAULT_IMPLEMENTATION=identity_map_prefetch_related_objects
+    )
+    def test_default_from_object(self):
+        self.config.set_default_prefetch_related_objects_implementation()
+        self.assertIs(
+            get_prefetch_related_objects(), identity_map_prefetch_related_objects
+        )
+
+    @override_settings(PREFETCH_UTILS_DEFAULT_IMPLEMENTATION=None)
+    def test_default_no_setting(self):
+        self.config.set_default_prefetch_related_objects_implementation()
+        self.assertIs(get_prefetch_related_objects(), original_prefetch_related_objects)
